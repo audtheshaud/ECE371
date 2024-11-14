4c525442df67b75105841161c96c487ab9970f0d
Aidan C <terminatedtank@gmail.com>
2024-11-14 11:22:56 -0500

    finished up image_client.py and image_server.py

--- Lab3/image_client.py
+++ Lab3/image_client.py
@@ -5,7 +5,8 @@ from RSA import generate_keypair, encrypt, decrypt
 import struct
 
 
-SERVER_IP = gethostbyname("DE1_SoC")
+# SERVER_IP = gethostbyname("DE1_SoC")
+SERVER_IP = gethostbyname("AidanEOS")
 PORT_NUMBER = 5000
 SIZE = 1024
 des_key = "secret_k"
@@ -19,26 +20,36 @@ mySocket = socket(AF_INET, SOCK_DGRAM)
 message = "hello"
 
 # first generate the keypair
-# get these two numbers from the excel file
-p = 1297273
-q = 1297651
+# get these two numbers from the excel file for seat 5B:  p - 1297211	q - 1297601
+p = 1297211
+q = 1297601
 ###################################your code goes here#####################################
 # generate public and private key from the p and q values
-public = [0, 0]
-private = [0, 0]
+public, private = generate_keypair(p, q)  # make keypairs
 # send key
 
-message = "public_key: %d %d" % (public[0], public[1])
+message = "public_key: %d %d private_key: %d %d" % (
+    public[0],
+    public[1],
+    private[0],
+    private[1],
+)  # send over private and public keys
 mySocket.sendto(message.encode(), (SERVER_IP, PORT_NUMBER))
 # send des_key
 message = "des_key"
 mySocket.sendto(message.encode(), (SERVER_IP, PORT_NUMBER))
 ###################################your code goes here#####################################
 # encode the DES key with RSA and save in DES_encoded, the value below is just an example
-des_encoded = ["2313", "3231", "532515", "542515", "5135151", "31413", "15315", "14314"]
+des_encoded = []
+for char in des_key:
+    encrypted_char = str(encrypt(public, char))
+    des_encoded.append(encrypted_char)
+
+# old code
+# des_encoded = ["2313", "3231", "532515", "542515", "5135151", "31413", "15315", "14314"]
 [mySocket.sendto(code.encode(), (SERVER_IP, PORT_NUMBER)) for code in des_encoded]
 # read image, encode, send the encoded image binary file
-file = open(r"penguin.jpg", "rb")
+file = open(r"Lab3/penguin.jpg", "rb") #relative path, need to update
 data = file.read()
 file.close()
 ###################################your code goes here#####################################
@@ -46,7 +57,14 @@ file.close()
 # set cbc to False when performing encryption, you should use the des class
 # coder=des.des(), use bytearray to send the encryped image through network
 # r_byte is the final value you will send through socket
+
+# code taken from ECE371/lab2/main.py
+coder = des.des()
+r = coder.encrypt(des_key, data, cbc=False)  # encrypted image
+# write the encrypted image into file
 r_byte = bytearray()
+for x in r:
+    r_byte += bytes([ord(x)])
 
 # send image through socket
 mySocket.sendto(bytes(r_byte), (SERVER_IP, PORT_NUMBER))
--- Lab3/image_server.py
+++ Lab3/image_server.py
@@ -1,16 +1,17 @@
-from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
-import sys
 import re
 import struct
-from RSA import decrypt
-import des
+import sys
+from socket import AF_INET, SOCK_DGRAM, gethostbyname, socket
 
+import des
+from RSA import decrypt
 
 PORT_NUMBER = 5000
 SIZE = 8192
 
 # hostName = gethostbyname( '192.168.1.3' )
-hostName = gethostbyname("DE1_SoC")
+# hostName = gethostbyname("DE1_SoC")
+hostName = gethostbyname("AidanEOS")
 
 mySocket = socket(AF_INET, SOCK_DGRAM)
 mySocket.bind((hostName, PORT_NUMBER))
@@ -24,13 +25,26 @@ while True:
     if data.find("public_key") != -1:  # client has sent their public key\
         ###################################your code goes here#####################################
         # retrieve public key and private key from the received message (message is a string!)
-        public_key_e = 0
-        public_key_n = 0
+
+        # code taken from ./chat_server.py
+        keys = re.search(
+            r"public_key: (\d+) (\d+) private_key: (\d+) (\d+)", data
+        )  # use regex to find message of key
+        public_key_e = int(keys.group(1))
+        public_key_n = int(keys.group(2))  # extract private and public keys
+        private_key = (int(keys.group(3)), int(keys.group(4)))
+
         print("public key is : %d, %d" % (public_key_e, public_key_n))
     elif data.find("des_key") != -1:  # client has sent their DES key
         ###################################your code goes here####################
         # read the next 8 bytes for the DES key by running (data,addr) = mySocket.recvfrom(SIZE) 8 times and then decrypting with RSA
-        des_key = "dummyval"
+        for _ in range(8):
+            (data, addr) = mySocket.recvfrom(SIZE)
+            encrypted_byte = int(
+                data.decode()
+            )  # collect part of key and convert to int
+            decrypted_byte = decrypt(private_key, encrypted_byte)  # decrypt
+            des_key += decrypted_byte  # append to des_key
         print("DES key is :" + des_key)
         # now we will receive the image from the client
         (data, addr) = mySocket.recvfrom(SIZE)
@@ -40,7 +54,18 @@ while True:
         # perform des decryption using des.py
         # coder=des.des()
         # the final output should be saved in a byte array called rr_byte
+
+        # code taken from ECE371/lab2/main.py and modified with des_key_decoded = des_key and r = data
+        decoder = des.des()
+        des_key_decoded_str = ""
+        for i in des_key:
+            des_key_decoded_str = des_key_decoded_str + str(i)
+        rr = decoder.decrypt(
+            des_key, data, cbc=False
+        )  # this is in string  format, must convert to byte format
         rr_byte = bytearray()
+        for x in rr:
+            rr_byte += bytes([ord(x)])
         # write to file to make sure it is okay
         file2 = open(r"penguin_decrypted.jpg", "wb")
         file2.write(bytes(rr_byte))
